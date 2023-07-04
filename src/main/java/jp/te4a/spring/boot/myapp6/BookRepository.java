5c7520ce6244e3c9cf0befeea5d17f9ab3a71ea3
200308 <200308@e0601-04.JC-21.local>
2023-07-04 12:08:49 +0900

    初回コミット

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/BookRepository.java
@@ -0,0 +1,22 @@
+package jp.te4a.spring.boot.myapp6;
+
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+@Repository
+public class BookRepository {
+	private final ConcurrentMap<Integer, BookBean> bookMap
+									= new ConcurrentHashMap<>();
+	
+	public BookBean save(BookBean bookBean) {
+		return bookMap.put(bookBean.getId(), bookBean);
+		}
+		public void delete(Integer bookId) {
+		bookMap.remove(bookId);
+		}
+		public List<BookBean> findAll() {
+		return new ArrayList<>(bookMap.values());
+		}
+		}
